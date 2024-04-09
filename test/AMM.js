0aeebe197df2cb06427249367b80fe2f6daa5df9
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2024-04-09 14:10:26 -0500

    Add Liquidity

--- test/AMM.js
+++ test/AMM.js
@@ -8,16 +8,27 @@ const tokens = (n) => {
 const ether = tokens
 
 describe('AMM', () => {
-  let accounts, deployer, token1, token2, amm
+  let accounts, deployer, token1, token2, amm, liquidityProvider
 
   beforeEach(async () => {
+    // Set up accounts
     accounts = await ethers.getSigners()
     deployer = accounts[0]
+    liquidityProvider = accounts[1]
 
+    // Deply token
     const Token = await ethers.getContractFactory('Token')
     token1 = await Token.deploy('Dapp University', 'DAPP', '1000000')
     token2 = await Token.deploy('USD Token', 'USD', '1000000')
 
+    // Send tokens to liquidity provider
+    let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    // Deploy AMM
     const AMM = await ethers.getContractFactory('AMM')
     amm = await AMM.deploy(token1.address, token2.address)
   })
@@ -31,8 +42,71 @@ describe('AMM', () => {
     it('tracks token1 address', async () => {
       expect(await amm.token1()).to.equal(token1.address)
     })
+
     it('tracks token2 address', async () => {
       expect(await amm.token2()).to.equal(token2.address)
+    })
+  })
+
+  describe('Swapping Tokens', () => {
+    let amount, transaction, result
+
+    it('facilitates swaps', async () => {
+      // Deployer approves 100k tokens
+      amount = tokens(100000)
+
+      transaction = await token1.connect(deployer).approve(amm.address, amount)
+      await transaction.wait()
+
+      transaction = await token2.connect(deployer).approve(amm.address, amount)
+      await transaction.wait()
+
+      // Deployer adds liquidity
+      transaction = await amm.connect(deployer).addLiquidity(amount, amount)
+      await transaction.wait()
+
+      // AMM receives tokens
+      expect(await token1.balanceOf(amm.address)).to.equal(amount)
+      expect(await token2.balanceOf(amm.address)).to.equal(amount)
+
+      expect(await amm.token1Balance()).to.equal(amount)
+      expect(await amm.token2Balance()).to.equal(amount)
+
+      // Check deployer has 100 shares
+      expect(await amm.shares(deployer.address)).to.equal(tokens(100))
+
+      // Check pool has 100 total shares
+      expect(await amm.totalShares()).to.equal(tokens(100))
+
+      /////////////////////////////////////////////////////////
+      // Liquidity provider adds more liquidity
+      //
+
+      // Liquidity provider approves 50k tokens
+      amount = tokens(50000)
+
+      transaction = await token1.connect(liquidityProvider).approve(amm.address, amount)
+      await transaction.wait()
+
+      transaction = await token2.connect(liquidityProvider).approve(amm.address, amount)
+      await transaction.wait()
+
+      // Calculate token2 deposit amount
+      let token2Deposit = await amm.calculateToken2Deposit(amount)
+
+      // Liquidity provider adds liquidity
+      transaction = await amm.connect(liquidityProvider).addLiquidity(amount, token2Deposit)
+      await transaction.wait()
+
+      // Liquidity provider should have 50 shares
+      expect(await amm.shares(liquidityProvider.address)).to.equal(tokens(50))
+
+      // Deployer should still have 100 shares
+      expect(await amm.shares(deployer.address)).to.equal(tokens(100))
+
+      // Pool should have 150 shares
+      expect(await amm.totalShares()).to.equal(tokens(150))
+
     })
   })
 
