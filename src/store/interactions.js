bd83d70fc5bbb3dd972988f2b5084afa7fae1a98
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2024-04-12 23:16:57 -0500

    Adds navbar

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -2,7 +2,7 @@ import { ethers } from 'ethers'
 
 import { setProvider, setNetwork, setAccount } from './reducers/provider'
 import { setContracts, setSymbols, balancesLoaded } from './reducers/tokens'
-import { setContract } from './reducers/amm'
+import { setContract, sharesLoaded } from './reducers/amm'
 
 import TOKEN_ABI from '../abis/Token.json';
 import AMM_ABI from '../abis/AMM.json';
@@ -23,11 +23,11 @@ export const loadNetwork = async (provider, dispatch) => {
 }
 
 export const loadAccount = async (dispatch) => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const account = ethers.utils.getAddress(accounts[0])
-    dispatch(setAccount(account))
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+  const account = ethers.utils.getAddress(accounts[0])
+  dispatch(setAccount(account))
 
-    return account
+  return account
 }
 
 // ----------------------------------------------------------------
@@ -50,12 +50,34 @@ export const loadAMM = async (provider, chainId, dispatch) => {
 
 // ----------------------------------------------------------------
 // LOAD BALANCES & SHARES
-export const loadBalances = async (tokens, account, dispatch) => {
+export const loadBalances = async (amm, tokens, account, dispatch) => {
 	const balance1 = await tokens[0].balanceOf(account)
 	const balance2 = await tokens[1].balanceOf(account)
 
-	dispatch(balancesLoaded(
-		balance1,
-		balance2
-	))
+	dispatch(balancesLoaded([
+		ethers.utils.formatUnits(balance1.toString(), 'ether'),
+		ethers.utils.formatUnits(balance2.toString(), 'ether')
+	]))
+
+	const shares = await amm.shares(account)
+	dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), 'ether')))
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
